92a04dd901abead29f2bc57a8af517104d50c8dd
Nikita <amarnikita09@gmail.com>
2022-04-06 20:22:18 +0530

    Refactored: to calcalate daily wage using switch case

--- EmployeePayroll.sh
+++ EmployeePayroll.sh
@@ -1,20 +1,25 @@
-#!/bin/bash -x
+
+
 
 wagePerhr=100;
 numOfhrs=0;
 isFullTime=1;
 isPartTime=2;
+totalSalary=0;
+numOfWorkingDays=21;
+noOfDaysworked=0;
+for ((day=1; day<= $numOfWorkingDays; day++ ))
+do
 isRandomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $isRandomCheck ];
-then 
-     numOfhrs=8;
-elif [ $isPartTime -eq $isRandomCheck ];
-then
-     numOfhrs=4;
-else
-     numOfhrs=0;
-fi   
- 
+case $isRandomCheck in
+   $isFullTime)
+              numOfhrs=8;;
+   $isPartTime)
+              numOfhrs=4;;
+   *)
+              numOfhrs=0;;
+esac
 salary=$(($wagePerhr*$numOfhrs));
-echo $salary;
-
+totalSalary=$(($salary+$totalSalary));
+done
+echo $totalSalary
